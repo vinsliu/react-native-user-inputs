93c2a1bf66b554d5183967baaa6bb438a547307e
Vincent <vincent00lav@outlook.com>
2025-07-08 15:11:59 +0200

    Récupération de données

--- App.tsx
+++ App.tsx
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, TextInput, TextInputProps, View } from "react-native";
+import styles from "./styles";
+import { useState } from "react";
 
-export default function App() {
+type InputProps = TextInputProps & {
+  label: string;
+};
+
+function Input(props: InputProps) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.textInputContainer}>
+      <Text style={styles.textInputLabel}>{props.label}</Text>
+      <TextInput style={styles.textInput} {...props} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function CollectingTextInput() {
+  const [changedText, setChangedText] = useState("");
+  const [submittedText, setSubmittedText] = useState("");
+
+  return (
+    <View style={styles.container}>
+      <Input label="Basic Text Input" />
+      <Input label="Password Input:" secureTextEntry />
+      <Input label="Return Key:" returnKeyType="search" />
+      <Input label="Placeholder Text:" placeholder="Search" />
+      <Input
+        label="Input Events:"
+        onChangeText={(e) => {
+          setChangedText(e);
+        }}
+        onSubmitEditing={(e) => {
+          setSubmittedText(e.nativeEvent.text);
+        }}
+        onFocus={() => {
+          setChangedText("");
+          setSubmittedText("");
+        }}
+      />
+      <Text>Changed: {changedText}</Text>
+      <Text>Submitted: {submittedText}</Text>
+    </View>
+  );
+}
---
+++ styles.ts
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "ghostwhite",
+    padding: 10,
+    paddingTop: 80,
+  },
+  textInputContainer: {
+    alignSelf: "stretch",
+    marginBlock: 30,
+  },
+  textInputLabel: {
+    marginBlock: 4,
+  },
+  textInput: {
+    backgroundColor: "white",
+    height: 40,
+    fontSize: 11,
+  },
+});
